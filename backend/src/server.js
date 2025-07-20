0a68e2f96bb25fa552ba1f06510d962ebfac2adf
ARUN-L-KUMAR <arunkumar582004@gmail.com>
2025-07-20 16:58:51 +0530

    Prepare for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,10 +7,12 @@ require('dotenv').config();
 const server = http.createServer(app);
 initSocket(server);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    server.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    server.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch(console.error);
